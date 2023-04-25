448815c0aff505dd9811766ec4910741f7b91209
yamazaki <yamazaki.160222@hotmail.com>
2023-04-25 19:05:08 +0900

    SetPlayerをpublicにしました

--- Assets/yamazaki/Scripts_Y/SetPlayer.cs
+++ Assets/yamazaki/Scripts_Y/SetPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SetPlayer : MonoBehaviour
 {
-    GameObject player;
+    public GameObject player;
     Transform tra;
 
     // Start is called before the first frame update

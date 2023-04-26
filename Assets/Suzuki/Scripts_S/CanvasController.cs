1a17422825f2569b8065c18767a2a80846b6bee8
suzuki <yukaken1994@gmail.com>
2023-04-26 18:19:32 +0900

    UI調整

--- Assets/Suzuki/Scripts_S/CanvasController.cs
+++ Assets/Suzuki/Scripts_S/CanvasController.cs
@@ -9,9 +9,12 @@ public class CanvasController : MonoBehaviour
     public GameObject player;
     public Text timeText;
     public Text lifeText;
+    public Text mainText;
 
     GameCon gameCon;
     CharCon_Y charCon_Y;
+    bool a_flag;
+    float a_color;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +22,26 @@ public class CanvasController : MonoBehaviour
         gameCon = mainCam.GetComponent<GameCon>();
         player = mainCam.GetComponent<CameraCon>().charaLookAtPosition.gameObject;
         charCon_Y = player.GetComponent<CharCon_Y>();
+
+        mainText.text = "Start!!";
+        a_flag = true;
+        a_color = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        getGameTime();
-        getLife();
+        TextClear(a_flag);
+        GetGameTime();
+        GetLife();
+
+        if (charCon_Y.GetIsGoal() == true)
+        {
+            TextApper(a_flag);
+        }
     }
 
-    void getGameTime()//実時間取得
+    void GetGameTime()//実時間取得
     {
         //float time = gameCon.GameTime();
         //以下2行、代理文
@@ -42,11 +55,46 @@ public class CanvasController : MonoBehaviour
             consoleTime = time;
         }
     }
-    void getLife()
+    void GetLife()
     {
         int defaultLife = charCon_Y.DefaultLife();
         int life = charCon_Y.Life();
         lifeText.text = "Life:" + life + "/" + defaultLife;
+    }
+
+    //mainTextを透明にしていく
+    void TextClear(bool a_flag)
+    {
+        //a_flagがtrueの間実行する
+        if (a_flag)
+        {
+            //テキストの透明度を変更する
+            mainText.color = new Color(0, 0, 0, a_color);
+            a_color -= Time.deltaTime / 2;
+            //透明度が0になったら終了する。
+            if (a_color < 0)
+            {
+                a_color = 0;
+                a_flag = false;
+            }
+        }
+    }
+
+    void TextApper(bool a_flag)
+    {
+        //a_flagがfalseの間実行する
+        if (!a_flag)
+        {
+            //テキストの透明度を変更する
+            mainText.color = new Color(0, 0, 0, a_color);
+            a_color += Time.deltaTime / 2;
+            //透明度が1になったら終了する。
+            if (a_color > 1)
+            {
+                a_color = 1;
+                a_flag = true;
+            }
+        }
     }
 
 }
